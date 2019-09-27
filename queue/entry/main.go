b0d663829b1d22ff7d9f331e175fd3a9421b2139
huangyz0918 <huangyz0918@gmail.com>
2019-09-27 15:50:59 +0800

    ✨ feat:

--- queue/entry/main.go
+++ queue/entry/main.go
@@ -1,5 +1,18 @@
 package main
 
+import (
+	"../queue"
+	"fmt"
+)
+
 func main() {
-	
+	q := queue.Queue{1}
+	q.Push(2)
+	q.Push(3)
+
+	fmt.Println(q.Pop())
+	fmt.Println(q.Pop())
+	fmt.Println(q.IsEmpty())
+	fmt.Println(q.Pop())
+	fmt.Println(q.IsEmpty())
 }
